e844b78b0c5aa03fe0b33b14fefdf153ea0cfa5b
Chris Skaaning <c@rpediem.com>
2020-10-16 23:57:48 +0100

    Moved things around

--- deployment/import_tf.sh
+++ deployment/import_tf.sh
@@ -3,12 +3,12 @@ rm plan.tf || true
 
 ../terraform init
 
-# ../terraform import module.games.aws_cloudfront_origin_access_identity.default[0] EIMIIXLMVEYV || true
+../terraform import module.games.aws_cloudfront_origin_access_identity.default[0] E12W39SWYQX4MM || true
 ../terraform import module.games.module.logs.aws_s3_bucket.default[0] skaaningninja-prod-games-logs || true
 ../terraform import module.games.aws_s3_bucket_policy.default[0] skaaningninja-prod-games-origin || true
 ../terraform import module.games.module.logs.aws_s3_bucket_public_access_block.default[0] skaaningninja-prod-games-logs || true
 ../terraform import module.games.aws_s3_bucket.origin[0] skaaningninja-prod-games-origin || true
-# ../terraform import module.games.aws_cloudfront_distribution.default E2PAE9UZIO3W9O || true
+../terraform import module.games.aws_cloudfront_distribution.default E1LGWUXYTVR36A || true
 
 ../terraform import aws_route53_record.games Z6J8SXXTH96JX_games.skaaning.ninja_A || true
 
