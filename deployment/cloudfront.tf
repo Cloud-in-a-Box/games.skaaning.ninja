039787e6c710d59a6f697b614d6049a94c84b548
Chris Skaaning <c@rpediem.com>
2020-10-17 00:06:23 +0100

    Trying things with cloudfront

--- deployment/cloudfront.tf
+++ deployment/cloudfront.tf
@@ -10,7 +10,7 @@ module "games" {
   aliases                   = ["games.skaaning.ninja"]
   acm_certificate_arn       = "arn:aws:acm:us-east-1:368263227121:certificate/9ad7b542-4be1-4be6-b138-bd47f00dda40"
   parent_zone_id            = "Z6J8SXXTH96JX"
-  cors_allowed_methods      = ["GET", "OPTIONS", "HEAD"]
+  cors_allowed_methods      = ["GET", "HEAD"]
   cors_allowed_origins      = ["*"]
   cors_allowed_headers      = ["Authorization", "*"]
   bucket_domain_format      = "%s.s3.amazonaws.com"
